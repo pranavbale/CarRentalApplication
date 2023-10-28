9203827140623b90614072b1e05bcf940f88dd27
Pranav Bale <pranavbale99@gmail.com>
2023-10-28 19:44:18 +0530

    connecting to database and add security

---
+++ carRental/src/main/java/com/springBootProject/carRental/controller/demoController.java
@@ -0,0 +1,12 @@
+package com.springBootProject.carRental.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoController {
+    @GetMapping("/")
+    private String helloWorld() {
+        return "HelloWorld!!!";
+    }
+}
