01258751df91a8796e758644a354728ad01b4479
Pranav Bale <pranavbale99@gmail.com>
2023-11-01 16:32:31 +0530

    add booking added

--- carRental/src/main/java/com/springBootProject/carRental/controller/BookCarController.java
+++ carRental/src/main/java/com/springBootProject/carRental/controller/BookCarController.java
@@ -1,7 +1,15 @@
 package com.springBootProject.carRental.controller;
 
 
+import com.springBootProject.carRental.dto.BookCarRequest;
+import com.springBootProject.carRental.entity.BookCar;
+import com.springBootProject.carRental.exception.CarNotFoundException;
+import com.springBootProject.carRental.service.BookCarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bookCar")
 public class BookCarController {
 
-    
+    @Autowired
+    BookCarService bookCarService;
+
+    @PutMapping("/add")
+    private ResponseEntity<BookCar> addBooking(@RequestBody BookCarRequest bookCarRequest) throws CarNotFoundException {
+        return new ResponseEntity<>(bookCarService.addBooking(bookCarRequest), HttpStatus.CREATED);
+    }
 }
--- carRental/src/main/java/com/springBootProject/carRental/service/BookCarService.java
+++ carRental/src/main/java/com/springBootProject/carRental/service/BookCarService.java
@@ -1,12 +1,34 @@
 package com.springBootProject.carRental.service;
 
+import com.springBootProject.carRental.dto.BookCarRequest;
+import com.springBootProject.carRental.entity.BookCar;
+import com.springBootProject.carRental.entity.Car;
+import com.springBootProject.carRental.exception.CarNotFoundException;
 import com.springBootProject.carRental.repository.BookCarRepository;
+import com.springBootProject.carRental.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BookCarService {
 
     @Autowired
     BookCarRepository bookCarRepository;
+
+    @Autowired
+    CarRepository carRepository;
+
+    public BookCar addBooking(BookCarRequest bookCarRequest) throws CarNotFoundException {
+
+        BookCar bookcar = BookCar.build(Long.valueOf(0), bookCarRequest.getBookDate(), bookCarRequest.getStartDate(), bookCarRequest.getEndDate(), bookCarRequest.getCar());
+        Optional<Car> carOptional = carRepository.findById(bookcar.getCar().getCarId());
+
+        if (!carOptional.isPresent()) {
+            throw new CarNotFoundException("Car is not present with id " + bookcar.getCar().getCarId());
+        }
+
+        return bookCarRepository.save(bookcar);
+    }
 }
